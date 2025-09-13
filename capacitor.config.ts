52ad0e0a1e54c819bf8056aa925b05b07af895dc
Ritesh771 <riteshnvisonex@gmail.com>
2025-09-13 20:10:19 +0530

    Revert "feat: update Capacitor dependencies and add notification sounds"
    This reverts commit 8113f484e02fdbdaf054dbe7303e716e1ad293b9.

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,19 +3,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.example.myapp',
   appName: 'NeuroCampus',
-  webDir: 'dist',
-  plugins: {
-    Camera: {
-      allowEditing: true,
-      saveToGallery: true,
-      quality: 85
-    },
-    LocalNotifications: {
-      smallIcon: 'ic_stat_icon_config_sample',
-      iconColor: '#488AFF',
-      sound: 'notification.wav'
-    }
-  }
+  webDir: 'dist'
 };
 
 export default config;
